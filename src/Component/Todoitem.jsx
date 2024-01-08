e0d567da558073892a571ea57f0d69c129523056
RANJAN <141898565+mranja@users.noreply.github.com>
2024-01-08 13:38:58 +0530

    Add files via upload

---
+++ src/Component/Todoitem.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+class TodoItem extends React.Component{
+    constructor(){
+        super();
+    }
+
+    render(){
+        
+        let {e, index, updateItem, deleteItem} = this.props;
+        return (
+            <>
+                <div>
+                    <input type="text" 
+                    onChange={(e)=>{
+                        updateItem(e.target.value, index)
+                    }} 
+                    value={e}/>
+                    <button 
+                    onClick={()=>{
+                        deleteItem(index)
+                    }}>Delete Item</button>
+                </div>
+            </>
+        )
+    }
+}
+
+export default TodoItem;
